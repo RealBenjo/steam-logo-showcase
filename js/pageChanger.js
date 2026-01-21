aad40fef9de7bbb00569fe4c76e05a5e2291c59f
Benjamin Ipavec <benjamin.ipavec@gmail.com>
2026-01-21 21:07:27 +0100

    workable + added the entire purchase page - it does not support mobile devices :( - it's so bad :(

--- js/pageChanger.js
+++ js/pageChanger.js
@@ -1,21 +1,19 @@
-function changePage(pageID) {
-  // get the pages we dont want to see
-  const purchasePageMain = document.getElementById('purchase_page_main');
-  const responsivePageTemplateContent = document.getElementById('responsive_page_template_content');
-  const responsiveStoreNavCtn = document.getElementById('responsive_store_nav_ctn');
+// important elements
+const purchasePageMain = document.getElementById('purchase_page_main');
+const responsivePageTemplateContent = document.getElementById('responsive_page_template_content');
+const responsiveStoreNavCtn = document.getElementById('responsive_store_nav_ctn');
 
-  // hide them
-  purchasePageMain.style.display = 'none';
+function changePage(pageID) {
+  // hide the important elements
   responsivePageTemplateContent.style.display = 'none';
   responsiveStoreNavCtn.style.display = 'none';
 
-  // get the page we want to show and show it :o
-  const pageToShow = document.getElementById(pageID);
-  pageToShow.style.display = 'block';
-
-  console.log(pageToShow);
+  purchasePageMain.style.display = 'block';
 }
 
 function GoToAppPage() {
-  
+  purchasePageMain.style.display = 'none';
+
+  responsivePageTemplateContent.style.display = 'block';
+  responsiveStoreNavCtn.style.display = 'block';
 }
